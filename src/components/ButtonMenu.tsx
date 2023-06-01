375eb90052ec9803d762f355c4c8d219fea024f4
davidfontesfelix <davidfontes303@gmail.com>
2023-05-31 23:37:44 -0300

    concertando erro chato

--- src/components/ButtonMenu.tsx
+++ src/components/ButtonMenu.tsx
@@ -15,11 +15,11 @@ export default function ButtonMenu() {
   return (
     <button
       onClick={() => handleClickButtonMenu()}
-      className="absolute mr-6 mt-10 h-16 w-12 rounded-full bg-sky-900 smartphones:mr-2  flex items-center flex-col gap-[6px] justify-center border border-gray-600"
+      className="absolute mr-6 mt-10 flex h-16 w-12 flex-col items-center  justify-center gap-[6px] rounded-full border border-gray-600 bg-sky-900 smartphones:mr-2"
     >
-      <div className='w-7 h-[2px] rounded-lg bg-white'></div>
-      <div className='w-7 h-[2px] rounded-lg bg-white'></div>
-      <div className='w-7 h-[2px] rounded-lg bg-white'></div>
+      <div className="h-[2px] w-7 rounded-lg bg-white"></div>
+      <div className="h-[2px] w-7 rounded-lg bg-white"></div>
+      <div className="h-[2px] w-7 rounded-lg bg-white"></div>
     </button>
   )
 }
